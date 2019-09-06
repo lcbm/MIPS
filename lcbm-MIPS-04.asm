435d51c0e84c2d04b101aa49fe861ae99b7d92e6
Leonardo Melo <lcbm@cesar.school>
2019-09-06 10:28:39 -0300

    Add combinatory program (template)

---
+++ lcbm-MIPS-04.asm
@@ -0,0 +1,21 @@
+# CESAR School
+# Computer Science
+# Hardware Infrastructure
+# Professor Paulo Serra Filho
+# By Leonardo Castelo Branco de Melo
+# Exercise 04: Combinatory
+
+# MIPS Instruction Set   : github.com/MIPT-ILab/mipt-mips/wiki/MIPS-Instruction-Set
+# MIPS Reference Material: sites.fas.harvard.edu/~libe295/spring2018/slides/MIPS%20Instruction%20Set.pdf
+# MARS Syscall functions : courses.missouristate.edu/kenvollmar/mars/help/syscallhelp.html
+
+.data   # declaring a static data segment (data is fixed in size)
+
+.text   # instructions segment
+    .globl main
+
+    main:
+
+    print:
+
+    exit:
